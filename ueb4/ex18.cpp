8e75066283a95994991f4a652d277f681d1bf1fa
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2015-04-11 14:28:21 +0200

    ex18 finished

---
+++ ueb4/ex18.cpp
@@ -0,0 +1,100 @@
+#include "../ode/bla/bla.hpp"
+#include "../ode/ode.hpp"
+
+
+class Chemical_Reaction5 : public ODE_Function
+{
+private:
+  double _k1;
+  double _k2;
+  double _k3;
+  double _k4;
+  double _k5;
+
+public:
+  Chemical_Reaction5 ( double k1, double k2, double k3, double k4, double k5)
+    : _k1(k1), _k2(k2), _k3(k3), _k4(k4), _k5(k5)
+  { }
+
+  virtual void Eval (double time, const ngbla::Vector<> & y_old,
+                     ngbla::Vector<> & y_new) const
+  {
+    y_new(0) =  - _k1 * y_old(0) * y_old(1) - _k3 * y_old(0) * y_old(2)
+                + _k4 * y_old(2) * y_old(2);
+
+    y_new(1) =  - _k1 * y_old(0) * y_old(1) - _k2 * y_old(1) * y_old(2)
+                + _k5 * y_old(4);
+
+    y_new(2) =    _k1 * y_old(0) * y_old(1) - _k2 * y_old(1) * y_old(2)
+                + _k3 * y_old(0) * y_old(2) - 2 * _k4 * y_old(2) * y_old(2);
+
+    y_new(3) =    _k1 * y_old(0) * y_old(1) + _k2 * y_old(1) * y_old(2)
+                + _k4 * y_old(2) * y_old(2);
+
+    y_new(4) =    _k3 * y_old(0) * y_old(2) - _k5 * y_old(4);
+
+  }
+
+  virtual void EvalDfDy (double time,
+                         const ngbla::Vector<> & y,
+                         ngbla::Matrix<> & dfdy) const
+  {
+    dfdy = 0.0;
+
+    dfdy(0,0) = - _k1 * y(1) - _k3 * y(2);
+    dfdy(0,1) = - _k1 * y(0);
+    dfdy(0,2) = - _k3 * y(0) + 2 * _k4 * y(2);
+
+    dfdy(1,0) = - _k1 * y(1);
+    dfdy(1,1) = - _k1 * y(0) - _k2 * y(2);
+    dfdy(1,2) = - _k2 * y(1);
+    dfdy(1,4) =   _k5;
+
+    dfdy(2,0) =   _k1 * y(1) + _k3 * y(2);
+    dfdy(2,1) =   _k1 * y(0) - _k2 * y(2);
+    dfdy(2,2) = - _k2 * y(1) + _k3 * y(0) - 4 * _k4 * y(2);
+
+    dfdy(3,0) =   _k1 * y(1);
+    dfdy(3,1) =   _k1 * y(0) + _k2 * y(2);
+    dfdy(3,2) =   _k2 * y(1) + 2 * _k4 * y(2);
+
+    dfdy(4,0) =   _k3 * y(2);
+    dfdy(4,2) =   _k3 * y(0);
+    dfdy(4,4) = - _k5;
+  }
+
+};
+
+
+int main ()
+{
+  ExplicitEuler expl_euler;
+  ImprovedEuler impr_euler;
+
+  double t_0 = 0;
+  double t_end = 200;
+  double tol = 1e-3;
+  double alpha_min = 0.2;
+  double alpha_max = 2.0;
+  double beta = 0.9;
+  double h_start = 1e-3;
+  double h_min = 1e-7;
+  double h_max = 1;
+  double save_every = 1e-3;
+
+  Chemical_Reaction5 func(1.34, 1.6e9, 8.0e3, 4.0e7, 1.0);
+  ngbla::Vector<> y_0(5);
+  y_0(0) = 0.05;
+  y_0(1) = 1e-4;
+  y_0(2) = 1e-10;
+  y_0(3) = 0.1;
+  y_0(4) = 1e-4;
+
+  std::ofstream out("data/ex18.out");
+  ODESolverAdaptive (func, expl_euler, impr_euler, t_0, t_end, y_0, tol,
+                     alpha_min, alpha_max, beta, h_start, h_min, h_max,
+                     save_every, out);
+
+  return 0;
+}
+
